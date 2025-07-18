d7d3da4ba3c24dd7f7e157dd980019e5e0d70648
JesseAger <agerjesse6@gmail.com>
2025-07-18 20:47:03 +0300

    eventlisteners and validations

--- js-event-assignment/script.js
+++ js-event-assignment/script.js
@@ -1,1 +1,28 @@
 const button = document.getElementById("button")
+const form = document.getElementById("userForm")
+const username = document.getElementById("username")
+const phone = document.getElementById("phone")
+const password = document.getElementById("password")
+
+
+username.addEventListener('input', function(){
+    const val = username.value;
+    if (val.length < 5){
+        username.setCustomValidity("Name must be at least 5 letters");
+    } else{
+        username.setCustomValidity("");
+    }
+});
+
+button.addEventListener('click', () => {
+    alert ('Submitted successfully')
+});
+
+phone.addEventListener('input', function(){
+    const val = phone.value;
+    if (val.length>10 && val.length<10){
+        phone.setCustomValidity("Phone number must be exactlyy 10 digits");
+    } else{
+        phone.setCustomValidity("");
+    }
+});
